2ad7ba432c01b11b80de6eed03cd418446be9c05
Jennifer Stoos <jstoos@bitlaw.com>
2018-01-28 21:53:21 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -120,7 +120,7 @@ class MusicLibraryController
       alphabetized = Song.all.sort_by {|obj| obj.name}
 
       artist_name = alphabetized[song_number.to_i-1].artist.name
-      song_name = alphabetized[song_number-1][song_number.to_i-1].name
+      song_name = alphabetized[song_number.to_i-1][song_number.to_i-1].name
       sentence = "Playing #{song_name} by #{artist_name}"
 
 #
